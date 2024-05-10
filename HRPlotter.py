b85af1a21eb2d04a68d0eb07559d952c4f88f7d6
Jameelo <97760369+Jameelo@users.noreply.github.com>
2024-05-10 10:35:18 +0100

    Updated HRPlotter.py to a clean up unused code.

--- HRPlotter.py
+++ HRPlotter.py
@@ -1,5 +1,5 @@
-# This code will run on the base pc, and so can use python unrestricted.
-# Retrieve heart data for processing, proof of concept for diagnostics.
+# This code will run on the base pc, and so can use the full version python
+# Retrieve heart data and graph both time and series domain data.
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,22 +15,21 @@ y = []
 # Open data file
 fileDest = open("J:\\University\\ACSEY4\\Project Work\\Data\\Direct_PPG_Data.txt","r")
 
-# Load data into y axis variable
+# Load data into y axis variable, normalised since the value is a 16-bit integer
 for line in fileDest:
     y.append(float(line)/(2**16))
 
 fileDest.close()
 
-# Timeseries section
+# Timeseries manipulation section
 
 # Create time axis for 256 samples & clip y values to the nearest power of 2 (for fft efficiency)
-t = np.linspace(0,12.75,num=256)
-tt = np.linspace(0,0.05*len(y),num=len(y))
+t = np.linspace(0,0.05*len(y),num=len(y))
 yClip = y[0:256]
 
 gTitleXY("Heart Data",'Samples','16-bit Light Intensity')
 plt.yticks()
-plt.plot(tt,y)
+plt.plot(t,y)
 
 plt.show()
 
@@ -38,8 +37,8 @@ plt.show()
 
 # calculate the fourier transform and the frequencies from the calculated bins
 yFT = np.fft.fft(yClip)
-f = np.fft.fftfreq(t.shape[-1],0.05)
-yFTmag = []
+f = np.fft.fftfreq(t.shape[-1],0.05) # By inserting 0.05 seconds into this function, the frequency buckets will be plotted as Hz.
+yFTmag = [] # Array to store FFT magnitude in.
 
 for i,val in enumerate(yFT):
     yFTmag.append(np.linalg.norm(val))
